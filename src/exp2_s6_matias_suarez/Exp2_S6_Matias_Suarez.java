b068a4fab6516d636b8e57bbfa22ff56c341ff45
matisuarezm <mati.suarez@duocuc.cl>
2025-09-13 17:48:41 -0300

    Limpiamos comentarios clase main.java

--- src/exp2_s6_matias_suarez/Exp2_S6_Matias_Suarez.java
+++ src/exp2_s6_matias_suarez/Exp2_S6_Matias_Suarez.java
@@ -1,20 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package exp2_s6_matias_suarez;
 
 /**
- *
  * @author msuarez
  */
 public class Exp2_S6_Matias_Suarez {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
